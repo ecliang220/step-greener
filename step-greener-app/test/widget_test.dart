1eaf0c80d2cc6e66127ef004c8e3c2864580eba5
ksedai <ksedai@ncsu.edu>
2025-02-21 17:27:46 -0500

    Implemented SplashScreen and LoginPage

--- step-greener-app/test/widget_test.dart
+++ step-greener-app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:google_maps_in_flutter/main.dart';
+import 'package:google_maps_in_flutter/LoginPage.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
