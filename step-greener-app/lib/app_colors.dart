c2a9327114be960600e2a885f6a20ff6eb168d47
eliang <eliang@ncsu.edu>
2025-02-22 21:15:52 -0500

    Implement AppBar and Menu

---
+++ step-greener-app/lib/app_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Define custom colors
+class AppColors {
+  static const Color primaryBackground = Color(0xFF66C2A4);
+  static const Color secondaryBackground = Color(0xFFB2E2E2);
+  static const Color primaryText = Color(0xFF006D2F);
+  static const Color primaryBorder = Color(0xFF2CA25F);
+}
