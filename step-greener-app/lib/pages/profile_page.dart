265203524795afbcc6e1268ac898990351a2c85e
eliang <eliang@ncsu.edu>
2025-03-06 17:38:21 -0500

    Change profile page to use page template

--- step-greener-app/lib/pages/profile_page.dart
+++ step-greener-app/lib/pages/profile_page.dart
@@ -73,60 +73,10 @@ class _ProfilePageState extends State<ProfilePage> {
     return GestureDetector(
       onTap: () {
         if (navigateToSettings) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => SettingsPage()), // Navigate to Settings
-          );
-        }
-      },
-      child: Container(
-        margin: EdgeInsets.symmetric(vertical: 5),
-        padding: EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12), // Rounded corners
-        ),
-        child: Row(
-          children: [
-            Icon(icon, color: Color(0xFF006D2F)), // Icon on the left
-            SizedBox(width: 10),
-            Expanded(
-              child: RichText(
-                text: TextSpan(
-                  text: noColon ? label : '$label: ', // Remove ":" for Settings & Locations Visited
-                  style: TextStyle(
-                    color: Color(0xFF006D2F),
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                  ),
-                  children: [
-                    if (!noColon) // Only add value if ":" is present
-                      TextSpan(
-                        text: value,
-                        style: TextStyle(fontWeight: FontWeight.normal),
-                      ),
-                  ],
-                ),
-              ),
-            ),
-            if (hasArrow)
-              Icon(Icons.arrow_forward_ios, color: Color(0xFF006D2F), size: 16), // Right arrow for navigation
-          ],
-        ),
-      ),
-    );
-  }
-
-  /// Creates a profile info tile with an icon, label, and optional arrow
-  Widget profileTile(IconData icon, String label, {String value = '', bool hasArrow = false, bool navigateToSettings = false}) {
-    bool noColon = label == 'Settings' || label == 'Locations Visited'; // Exclude ":" for these labels
-
-    return GestureDetector(
-      onTap: () {
-        if (navigateToSettings) {
-          Navigator.push(
-            context,
-            MaterialPageRoute(builder: (context) => SettingsPage()), // Navigate to Settings
+          Navigator.pushReplacement(
+            context, MaterialPageRoute(
+              builder: ( context ) => PageTemplate(page: SettingsPage(), pageType: SettingsPage.pageType,)
+            )
           );
         }
       },
